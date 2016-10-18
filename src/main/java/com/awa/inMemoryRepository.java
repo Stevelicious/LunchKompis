bbbf9bbeb3787772589741490f614cf75eb8899d
Steven Hu <husteven.91@gmail.com>
2016-10-18 14:26:31 +0200

    Add Lunch and Usergroup classes

---
+++ src/main/java/com/awa/inMemoryRepository.java
@@ -0,0 +1,66 @@
+package com.awa;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.io.Serializable;
+
+/**
+ * Created by Steven Hu on 2016-10-18.
+ */
+public class inMemoryRepository implements CrudRepository{
+	
+	@Override
+	public Object save(Object o) {
+		return null;
+	}
+	
+	@Override
+	public Iterable save(Iterable iterable) {
+		return null;
+	}
+	
+	@Override
+	public Object findOne(Serializable serializable) {
+		return null;
+	}
+	
+	@Override
+	public boolean exists(Serializable serializable) {
+		return false;
+	}
+	
+	@Override
+	public Iterable findAll() {
+		return null;
+	}
+	
+	@Override
+	public Iterable findAll(Iterable iterable) {
+		return null;
+	}
+	
+	@Override
+	public long count() {
+		return 0;
+	}
+	
+	@Override
+	public void delete(Serializable serializable) {
+		
+	}
+	
+	@Override
+	public void delete(Object o) {
+		
+	}
+	
+	@Override
+	public void delete(Iterable iterable) {
+		
+	}
+	
+	@Override
+	public void deleteAll() {
+		
+	}
+}
