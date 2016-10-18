2a4445f788ee2b2b99077b86d28ad3c0aa2c35b1
Roberto <skinkpajen@gmail.com>
2016-10-18 10:48:32 +0200

    userclass

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,7 +1,59 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Roberto Angius on 2016-10-18.
  */
 public class User {
+    private long userID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String nickname;
+    private String password;
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
