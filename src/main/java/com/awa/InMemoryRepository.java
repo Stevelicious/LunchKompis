5c207e9be6bc39ed02ac2b7974d10bd3700085c7
Steven Hu <husteven.91@gmail.com>
2016-10-19 20:54:28 +0200

    Clean up in Lunch.java and InMemRepo

--- src/main/java/com/awa/InMemoryRepository.java
+++ src/main/java/com/awa/InMemoryRepository.java
@@ -1,9 +1,7 @@
 package com.awa;
 
 
-import javax.jws.soap.SOAPBinding;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
--- src/main/java/com/awa/Lunch.java
+++ src/main/java/com/awa/Lunch.java
@@ -1,13 +1,10 @@
 package com.awa;
 
-import com.sun.javafx.binding.StringFormatter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
