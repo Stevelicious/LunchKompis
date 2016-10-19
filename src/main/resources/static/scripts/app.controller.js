eac5e69d6ee646c49df5534dd50cb3da61ac1c83
Kajsa Lampa <kajsalampa@live.se>
2016-10-19 11:47:39 +0200

    json works

--- src/main/resources/static/scripts/app.controller.js
+++ src/main/resources/static/scripts/app.controller.js
@@ -4,13 +4,50 @@ if (!awa)
 if (!awa.controllers)
      awa.controllers = {};
 
-awa.controllers.AppController = function(repoService) {
+awa.controllers.AppController = function() {
 
     var self = this;
-    var dataSet = repoService.getLunches(); // awa.repo.getLunches();
+
+    var dummyData = {
+        "lunches": [
+            {
+                "title": "dejt",
+                "date": "10/11",
+                "time": "11:30",
+                "isPublic": false,
+                "place": "dovas",
+                "users": [
+                    "user1", "user2"
+                ]
+            },
+            {
+                "title": "business",
+                "date": "02/12",
+                "time": "12:00",
+                "isPublic": true,
+                "place": "pong",
+                "users": [
+                    "user9", "user2"
+                ]
+            },
+            {
+                "title": "breakup",
+                "date": "08/11",
+                "time": "11:00",
+                "isPublic": true,
+                "place": "mcdonalds",
+                "users": [
+                    "user1", "user6"
+                ]
+            }
+        ]
+    }
+    // var dataSet = repoService.getLunches(); // awa.repo.getLunches();
     this.search = function () {
     return "Works!"
     }
 
+
+    self.data = dummyData;
     self.message = "Kalle";
  }
--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -1,2 +1,4 @@
-angular.module("awa", ["repo.module"])
-    .controller("appCtrl", ["repoService", awa.controllers.AppController]);
+// angular.module("awa", ["repo.module"])
+//     .controller("appCtrl", ["repoService", awa.controllers.AppController]);
+angular.module("awa", [])
+    .controller("appCtrl", [awa.controllers.AppController]);
