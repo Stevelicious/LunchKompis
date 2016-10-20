18bb146b498768236fe3dd9bd893288d7f1c7d68
Roberto <skinkpajen@gmail.com>
2016-10-20 17:35:54 +0200

    add maps to lunches

--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -1,2 +1,45 @@
 angular.module('lunchApp', [])
-.controller('groupCtrl', ['$scope', '$http', lunchApp.controllers.AppController]);
+.controller('groupCtrl', ['$scope', '$http', lunchApp.controllers.AppController])
+    .controller('mapCtrl', function ($scope, $http) {
+        $http.get('/scripts/groups.json').success(function (data) {
+            $scope.locations = data.lunches;
+        });
+        $scope.setLoc = function (location) {
+            init(location.lat,location.lon);
+            console.log("funkar");
+            console.log(location.lat);
+            console.log(location.lon);
+        }
+
+        var zoom=18;
+
+        var map; //complex object of type OpenLayers.Map
+
+        //Initialise the 'map' object
+        function init(lat, lon) {
+            console.log(lat);
+            console.log(lon);
+
+            map = new OpenLayers.Map ("map", {
+                controls:[
+                    new OpenLayers.Control.Navigation(),
+                    new OpenLayers.Control.MousePosition(),
+                    new OpenLayers.Control.Attribution()],
+                maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
+                maxResolution: 156543.0339,
+                numZoomLevels: 19,
+                units: 'm',
+                projection: new OpenLayers.Projection("EPSG:900913"),
+                displayProjection: new OpenLayers.Projection("EPSG:4326")
+            } );
+
+            layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
+            map.addLayer(layerMapnik);
+
+            if( ! map.getCenter() ){
+                var lonLat = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+                map.setCenter (lonLat, zoom);
+            }
+        }
+
+    });
