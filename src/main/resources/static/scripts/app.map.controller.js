b7bad7c73a0b3a056814b410546f61eda2b190bc
Roberto <skinkpajen@gmail.com>
2016-10-24 09:50:50 +0200

    create map-controller

---
+++ src/main/resources/static/scripts/app.map.controller.js
@@ -0,0 +1,45 @@
+if (!lunchApp)
+    var lunchApp = {};
+
+if (!lunchApp.controllers)
+    lunchApp.controllers = {};
+
+
+lunchApp.controllers.MapController = function ($scope, $http) {
+
+    $http.get('/scripts/groups.json').success(function (data) {
+        $scope.locations = data.lunches;
+    });
+    $scope.setLoc = function (location) {
+        init(location.lat, location.lon);
+    }
+
+    var zoom = 18;
+    var map; //complex object of type OpenLayers.Map
+
+    //Initialise the 'map' object
+
+    function init(lat, lon) {
+        map = new OpenLayers.Map("map", {
+            controls: [
+                new OpenLayers.Control.Navigation(),
+                new OpenLayers.Control.MousePosition(),
+                new OpenLayers.Control.Attribution()],
+            maxExtent: new OpenLayers.Bounds(-20037508.34, -20037508.34, 20037508.34, 20037508.34),
+            maxResolution: 156543.0339,
+            numZoomLevels: 19,
+            units: 'm',
+            projection: new OpenLayers.Projection("EPSG:900913"),
+            displayProjection: new OpenLayers.Projection("EPSG:4326")
+        });
+
+        layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
+        map.addLayer(layerMapnik);
+
+        if (!map.getCenter()) {
+            var lonLat = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+            map.setCenter(lonLat, zoom);
+        }
+    }
+
+};
