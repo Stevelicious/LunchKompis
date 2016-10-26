72b45ed7576fddf016aa9b10477eb066db8de089
Math-ematics <junk@fernstedt.se>
2016-10-26 17:10:08 +0200

    Add styling and more frontend

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -121,3 +121,21 @@ function main() {
 
 }
 main();
+
+$("[name='my-checkbox']").bootstrapSwitch();
+
+//Select2
+$.getScript('http://cdnjs.cloudflare.com/ajax/libs/select2/3.4.8/select2.min.js', function() {
+
+    /* dropdown and filter select */
+    var select = $('#select2').select2();
+
+    /* Select2 plugin as tagpicker */
+    $("#grupper").select2({
+        closeOnSelect: false,
+        placeholder: "Klicka för att välja grupper"
+    });
+
+}); //script
+
+$(document).ready(function() {});
