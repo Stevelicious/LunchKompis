cc552e9ad3ce55a65de4ea1a5e1689cbf3d28836
Sirar Alsam <siral389@student.liu.se>
2016-10-27 20:39:40 +0200

    redirect back to login after logout:

--- src/main/java/com/awa/DbTestController.java
+++ src/main/java/com/awa/DbTestController.java
@@ -7,9 +7,19 @@ package com.awa;
 
 import com.awa.tables.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -55,5 +65,14 @@ public class DbTestController {
         return "Database connectivity seems ";
 
     }
+    @RequestMapping(value="/lout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
+    }
+
 
 }
